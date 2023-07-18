a782ae0b525e0e5e09028986378800550469fa4a
Iveta Havranova <ivetah009@gmail.com>
2023-07-18 10:40:56 +0200

    Java Output Formatitng

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
 public class Solution {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                System.out.print(s1);
+                for (int j=0; j<(15-s1.length());j++) {
+                   System.out.print(" "); 
+                }
+                for (int k=0; k<(3 -  String.valueOf(x).length());k++) {
+                   System.out.print("0"); 
+                }
+                System.out.println(x);
+            }
+            sc.close();
+            System.out.println("================================");
 
-	}
-
+    }
 }
+
+
+
